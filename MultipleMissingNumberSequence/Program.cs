1db34e635671e1d82211e14b8cf6fe013c1c20ff
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-04 11:39:27 +0530

    Missing number in sequence, XOR (setbit) approach implementation is pending

---
+++ MultipleMissingNumberSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿//TODO: User XOR approach to find the set of missing numbes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleMissingNumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 9,1, 2, 4, 5, 6,9,  7, 9 };
+            foreach(int item in MissingNumber(arr))
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        /*
+         * */
+        public static List<int> MissingNumber(int[] arr)
+        {
+            int[] sortedDistinctArray = arr.Distinct().ToArray();
+            Array.Sort(sortedDistinctArray);
+            return Enumerable.Range(sortedDistinctArray[0], sortedDistinctArray[sortedDistinctArray.Length - 1]).Except(sortedDistinctArray.Cast<int>()).ToList();
+        }
+    }
+}
