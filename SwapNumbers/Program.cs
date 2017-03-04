5c75b56035ebaeb2304d9fe11e93df59d33d9076
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-04 17:28:00 +0530

    Swaping number without temp variable and using XOR

---
+++ SwapNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SwapXOR(65, 43);
+            Console.ReadLine();
+        }
+
+        public static void Swap1(int x, int y)
+        {
+            x = x + y;
+            y = x - y;
+            x = x - y;
+
+            Console.WriteLine("x: " +  x + " , y: " +  y);
+        }
+
+        public static void SwapXOR(int x, int y)
+        {
+            x ^= y;
+            y ^= x;
+            x ^= y;
+
+            Console.WriteLine("x: " + x + " , y: " + y);
+        }
+    }
+}
