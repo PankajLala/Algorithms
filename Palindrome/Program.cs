7c71a92db2ee204267e22d851a91402842bfe6f2
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-04 12:28:46 +0530

    Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("super"));
+            Console.ReadLine();
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            int len = str.Length;
+            int middle = len / 2;
+           
+            for(int count = 0; count < middle; count++)
+            {
+                if(str[count] != str[len - 1 - count])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
