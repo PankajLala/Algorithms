a5527ae82659b62e2cb73629cd4f56064eafd9e0
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-04 12:17:15 +0530

    Reverse number - TODO recursive approach pending

---
+++ RevesetNumber/Program.cs
@@ -0,0 +1,52 @@
+﻿//TODO: Recursive approach
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevesetNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseIterative(765));
+            Console.ReadLine();
+        }
+
+        /* 
+         * Making char arry then reversing same and then creating string and then int. Phew bad space complexity
+        */
+        public static int ReverseUsingArray(int num)
+        {
+            string textValue = num.ToString().TrimStart('-');
+
+            char[] valueChars = textValue.ToCharArray();
+            Array.Reverse(valueChars);
+            string reversedValue = new string(valueChars);
+            int reversedInt = int.Parse(reversedValue);
+
+            if (num < 0)
+                reversedInt *= -1;
+
+            return reversedInt;
+        }
+
+        public static int ReverseIterative(int num)
+        {
+            int remainder=0, sum = 0;
+
+            while (num > 0)
+            {
+                remainder = num % 10;
+                sum = sum * 10 + remainder;
+                num = num / 10;
+            }
+
+            return sum;
+        }
+
+    }
+}
+
