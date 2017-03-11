f669b077bc531c20caefc00fc3af8796380cf0e3
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-11 21:16:20 +0530

    Insertion sort

---
+++ InsertSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertSort
+{
+    /* O(n2) 
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 1, 6, 2, 4, 3 };
+            InsertSort(ref arr);
+
+            foreach(var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public static void InsertSort(ref int[] arr)
+        {
+
+            int j, key;
+            for(int i = 1; i < arr.Length; i++) //Always start from the second element making it i
+            {
+                j = i - 1; //, and j one less then i
+                key = arr[i]; // capture the ith element as key
+
+                while(j>=0 && arr[j] > key) // till  the item on left of i are less then key and j >=0 
+                {
+                    arr[j + 1] = arr[j]; //keep on shifting left element to right
+                    j--;                 // also keep on moving towards left 
+                }
+
+                arr[j + 1] = key; // insert the key at the last position of j
+            }
+        }
+    }
+}
---
+++ InsertSort/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InsertSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("InsertSort")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4fc6127-c7a6-49a7-ac71-b3f5a23057e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
