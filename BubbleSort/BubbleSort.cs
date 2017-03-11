20e762ee204c1aa8d5bd7dd29d831b6c64db84dd
Pankaj Lala <pankaj.lala@xceedance.com>
2017-03-11 19:57:19 +0530

    Sorting algorithms

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+
+    //Time-complexity: O(n2);
+    //Space-Complexity: O(1)
+    //Stablility: Stable (a stable sort algo is one which preserves the order of objects with equal keys)
+    class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 5, 1, 6, 2, 4, 3 };
+            var len = arr.Length;
+
+            int temp = 0;
+
+            for(int i= 0; i < len-1; i++)
+            {
+                for(int j = 0; j < len-1 - i; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach(var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class QuickSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 1, 6, 2, 4, 3 };
+            IEnumerable<int> result = QuickSortAlgo(arr);
+            
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] arr2 = { 5, 1, 6, 2, 4, 3 };
+
+            List<int> arr1 = new List<int>(arr2);
+            List<int> result2 = QuickSortAlgo2(arr1);
+
+            foreach (var item in result2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> QuickSortAlgo(IEnumerable<int> i)
+        {
+            if (!i.Any()) return i;
+            var p = i.ElementAt(new Random().Next(0, i.Count() - 1));
+            return QuickSortAlgo(i.Where(x => x < p)).Concat(i.Where(x => x == p)).Concat(QuickSortAlgo(i.Where(x => x >  p)));
+        }
+
+        public static List<int> QuickSortAlgo2(List<int> i)
+        {
+            if (i.Count() < 2) return i;
+            var pivot = new Random().Next(i.Count());
+            int val = i[pivot];
+            var lesser = new List<int>();
+            var greater = new List<int>();
+
+            foreach(var item in i)
+            {
+                if(item != val){
+                    if (item < val) lesser.Add(item);
+                    if (item >= val) greater.Add(item);
+                }
+            }
+
+
+            var merged = QuickSortAlgo2(lesser);
+            merged.Add(val);
+            merged.AddRange(QuickSortAlgo2(greater));
+            return merged;
+
+        }
+    }
+}
